02a074c907d3676d41e9075d54ee116d7a9a5a45
Jure Lucian Boban <jlboban17@gmail.com>
2020-09-30 22:56:00 +0200

    Path update.

--- App/Config.php
+++ App/Config.php
@@ -5,7 +5,6 @@ ini_set('display_errors', 1);
 define("DS", DIRECTORY_SEPARATOR);
 define("BP", dirname(__DIR__) . DS);
 define("APP_ROOT", dirname(__FILE__, 2) . DS . "App" . DS);
-define("PUB", dirname(__FILE__, 2) . DS . "pub" . DS);
 define("URL_ROOT", "http://phpacademy.inchoo.io/");
 
 define("VIEW_PATH", APP_ROOT . "View" . DS);
