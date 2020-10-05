d54a268b4fe2929695f30c35da5b4ce1d0effcc6
Jure Lucian Boban <jlboban17@gmail.com>
2020-10-05 02:07:37 +0200

    Script and nav path update.

--- App/Config.php
+++ App/Config.php
@@ -6,7 +6,7 @@ define("DS", DIRECTORY_SEPARATOR);
 define("BP", dirname(__DIR__) . DS);
 define("APP_ROOT", dirname(__FILE__, 2) . DS . "App" . DS);
 define("PUB", dirname(__FILE__, 2) . DS . "pub" . DS);
-define("URL_ROOT", "http://eventzoneupgrade.com/~polaznik3/");
+define("URL_ROOT", "http://phpacademy.inchoo.io/~polaznik3/");
 
 define("VIEW_PATH", APP_ROOT . "View" . DS);
 define("TEMPLATE_PATH", APP_ROOT . "View" . DS . "Pages" . DS . "Templates" . DS);
