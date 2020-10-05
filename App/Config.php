381a2da56d2f07066b48e90a8bfb48d5c570196a
Jure Lucian Boban <jlboban17@gmail.com>
2020-10-05 03:25:28 +0200

    Nav

--- App/Config.php
+++ App/Config.php
@@ -3,7 +3,6 @@
 ini_set('display_errors', 1);
 
 define("DS", DIRECTORY_SEPARATOR);
-define("BP", dirname(__DIR__) . DS);
 define("APP_ROOT", dirname(__FILE__, 2) . DS . "App" . DS);
 define("PUB", dirname(__FILE__, 2) . DS . "pub" . DS);
 define("URL_ROOT", "http://phpacademy.inchoo.io/~polaznik3/");
--- pub/index.php
+++ pub/index.php
@@ -3,6 +3,8 @@
 require_once '../App/Config.php';
 require_once '../Core/Autoloader.php';
 
+define('BP', dirname(__DIR__) . DS);
+
 use Core\Autoloader;
 use App\Application;
 
